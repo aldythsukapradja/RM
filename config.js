eb77b86cce11fea09d7d69da49dfa7fbbb7742c4
Aldyth SUKAPRADJA <aldhyt.sukapradja@gmail.com>
2026-06-08 21:03:58 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+window.CONFIG = {
+  GEMINI_API_KEY: "AQ.Ab8RN6KfuPzixX2MVood_Ks-RS_pmm25PWpm0BwwzURoWcS5EA",
+  SHEETS_API_URL: "https://script.google.com/macros/s/AKfycby6gGEuATCTT1ysXZG9t4YS_4N8HwASJ8U7_HXfruRCEaspdxPNMvB4LU9hTsUnwcv7/exec"
+};
